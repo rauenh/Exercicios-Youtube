4fb94caf8dfeb89d7dda5d484fd65cc009d1a0c6
rauenh <hingryd.rauen@alumni.usp.br>
2022-10-17 10:30:41 -0300

    Exercicio 28

--- Ex_28.py
+++ Ex_28.py
@@ -1,2 +1,12 @@
 #028: Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário
 # descobrir qual foi o número escolhido pelo computador. -O programa deverá escrever na tela se o usuário venceu ou perdeu.
+from random import randrange
+
+num_usuario = input("Digite um numero de 0 a 5\n")
+num = randrange(0,5)
+print(num)
+
+if num_usuario == num:
+    print("Você venceu!")
+else:
+    print("Você perdeu!")
