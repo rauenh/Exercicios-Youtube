a8a871da349d605706e7d613d00057091bd4d4aa
rauenh <hingryd.rauen@alumni.usp.br>
2022-11-24 09:58:24 -0300

    Versão correta do exercicio utilizando a conversão prória do Python

---
+++ Ex_37_Corrigido.py
@@ -0,0 +1,16 @@
+numero = int(input("Digite aqui um número inteiro qualquer:\n"))
+print('''Escolha uma das bases a seguir para conversão:
+1 - Converte para Binário
+2 - Converte para Octal
+3 - Converte para Hexadecimal''')
+menu = int(input("Digite aqui o número da sua opção:\n"))
+
+if menu ==1:
+    print("O número {} convertido para a base binária é {}".format(numero, bin(numero)[2:]))
+
+elif menu ==2:
+    print("O número {} convertido para a base octal é {}".format(numero,oct(numero)[2:]))
+elif menu ==3:
+    print("O número {} convertido para a base hexadecimal é {}".format(numero,hex(numero)[2:]))
+else:
+    print("Digite uma opção válida")
