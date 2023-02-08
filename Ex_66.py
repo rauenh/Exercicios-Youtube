605853cff3b71073c92d7f839e4a1b849da7f7db
rauenh <hingryd.rauen@alumni.usp.br>
2023-02-08 19:18:50 -0300

    ostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).

---
+++ Ex_66.py
@@ -0,0 +1,13 @@
+#66: Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999
+# , que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
+
+inteiro = int(input("Digite aqui um número inteiro positivo: "))
+cont = soma = 0
+
+while True:
+    soma += inteiro
+    cont += 1
+    inteiro = int(input("Digite aqui um número inteiro positivo: "))
+    if inteiro == 999:
+        break
+print(f'Foram digitados {cont} números. A soma entre eles é de {soma}')
