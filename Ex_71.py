3b0fb2c73beb9230ce63b164c3bbc1738c2d2deb
rauenh <hingryd.rauen@alumni.usp.br>
2023-02-09 22:59:00 -0300

    Simular um caixa eletrônico, alterações no código

--- Ex_71.py
+++ Ex_71.py
@@ -8,38 +8,29 @@ print('*-' * 20)
 cedulas_50 = resto_50 = cedulas_20 = resto_20 = cedulas_10 = resto_10 = cedulas_1 = resto_1 = 0
 while True:
     valor = int(input("Digite aqui o valor a ser sacado: R$"))
-    if valor // 50 != 0:
+    if valor//50 != 0:
         cedulas_50 = valor // 50
         resto_50 = valor%50
-        if resto_50 // 20 != 0:
-            cedulas_20 = resto_50 // 20
-            resto_20 = resto_50%20
-        if resto_20 // 10 != 0:
-            cedulas_10 = resto_20 // 10
-            resto_10 = resto_20%10
-        if resto_10 // 1 != 0:
-            cedulas_1 = resto_10 // 1
+        cedulas_20 = resto_50//20
+        resto_20 = resto_50%20
+        cedulas_10 = resto_20//10
+        resto_10 = resto_20%10
+        cedulas_1 = resto_10//1
         print(f'Você poderá sacar com {cedulas_50} cédulas de 50 reais, {cedulas_20} cédulas de 20 reais, {cedulas_10} cédulas de 10 reais e {cedulas_1} cédulas de 1 real.')
-    elif valor // 20 != 0:
+    elif valor//20 != 0:
         cedulas_20 = valor // 20
-        resto_20 =  valor%20
-        if resto_20 // 10 != 0:
-            cedulas_10 = resto_20 // 10
-            resto_10 = resto_20%10
-            if resto_10 // 1 != 0:
-                cedulas_1 = resto_10 // 1
-            print(f'Você poderá sacar com {cedulas_20} cédulas de 20 reais, {cedulas_10} cédulas de 10 reais e {cedulas_1} cédulas de 1 real.')
-    elif valor // 10 != 0:
-        cedulas_10 = valor //10
-        resto_10 = valor%10
-        if resto_10 // 1 != 0:
-            cedulas_1 = resto_10 // 1
-            print(f'Você poderá sacar com {cedulas_10} cédulas de 10 reais e {cedulas_1} cédulas de 1 real.')
-
+        resto_20 = valor % 20
+        cedulas_10 = resto_20 // 10
+        resto_10 = resto_20 % 10
+        cedulas_1 = resto_10 // 1
+        print(f'Você poderá sacar com {cedulas_20} cédulas de 20 reais, {cedulas_10} cédulas de 10 reais e {cedulas_1} cédulas de 1 real.')
+    elif valor//10 != 0:
+        cedulas_10 = valor// 10
+        resto_10 = valor% 10
+        cedulas_1 = resto_10 // 1
     else:
         cedulas_1 = valor//1
         print(f'Você poderá sacar com {cedulas_1} cédulas de 1 real.')
-
     menu = ' '
     while menu not in 'SN':
         menu = str(input("Deseja realizar outra operação? [S/N] ")).upper().strip()[0]
