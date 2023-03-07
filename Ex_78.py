ebd2cb0cd3eba7dfb3eb02a9bcbfba0f78dfd56e
rauenh <hingryd.rauen@alumni.usp.br>
2023-03-07 13:48:29 -0300

    Aula sobre listas no python

---
+++ Ex_78.py
@@ -0,0 +1,6 @@
+#078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
+# No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+
+lista = [int(input("Digite um número: ")), int(input("Digite um número: ")), int(input("Digite um número: ")), int(input("Digite um número: ")), int(input("Digite um número: "))]
+print(lista)
+print(f'O maior número da lista é {max(lista)} na posição {lista.index(max(lista))} e o menor número é {min(lista)} na posição {lista.index(min(lista))}')
