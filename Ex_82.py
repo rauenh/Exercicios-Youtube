3210cac62f719204eb8dc6be18c616b0ed13926f
rauenh <hingryd.rauen@alumni.usp.br>
2023-03-08 15:21:54 -0300

    Aula sobre listas no python

---
+++ Ex_82.py
@@ -0,0 +1,21 @@
+#082: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão
+# conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas
+
+lista = []
+lista_impar = []
+lista_par = []
+while True:
+    menu = ' ' #Para o menu sempre iterar novamente sua variável tem que ficar dentro
+    numeros = lista.append(int(input("Digite um número: ")))
+    while menu not in 'SN':
+        menu = str(input("Você deseja adicionar mais números? [S/N]: ")).upper().strip()
+    if menu == 'N':
+        break
+for elementos in lista:
+    if elementos%2 == 0:
+        lista_par.append(elementos)
+    if elementos%2 != 0:
+        lista_impar.append(elementos)
+print(f'Os números digitados foram {lista}.)
+print(f' Os números ímpares são: {lista_impar})
+print(f'Os números pares são: {lista_par})
